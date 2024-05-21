114c3af5f9036e98be95fffd3c75a22ddcefd09d
Róger Roca Arano <63246718+rogerrocaarano@users.noreply.github.com>
2024-05-21 18:48:02 -0400

    Get connection string from ENV variable DB_CONNECTION_STRING by default.

--- Program.cs
+++ Program.cs
@@ -4,9 +4,21 @@ using c18_98_m_csharp.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Get proper connection string from environment variable or appsettings.json
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ??
-                       throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+                       throw new InvalidOperationException(
+                           "Connection string 'DefaultConnection' not found."
+                           );
+
+if (connectionString == "GET_FROM_ENV")
+{
+    connectionString = Environment.GetEnvironmentVariable("DB_CONNECTION_STRING") ??
+                       throw new InvalidOperationException(
+                           "Environment variable 'DB_CONNECTION_STRING' not found."
+                           );
+}
+
+// Add services to the container.
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
