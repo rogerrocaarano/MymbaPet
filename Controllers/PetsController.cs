1841481c7804ae195c2cf9356b280b5df0771624
Róger Roca Arano <63246718+rogerrocaarano@users.noreply.github.com>
2024-05-24 18:37:31 -0400

    Initial PetsController implementation.

---
+++ Controllers/PetsController.cs
@@ -0,0 +1,158 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using c18_98_m_csharp.Data;
+using c18_98_m_csharp.Models;
+
+namespace c18_98_m_csharp.Controllers
+{
+    public class PetsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PetsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Pets
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Pets.ToListAsync());
+        }
+
+        // GET: Pets/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var pet = await _context.Pets
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (pet == null)
+            {
+                return NotFound();
+            }
+
+            return View(pet);
+        }
+
+        // GET: Pets/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Pets/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Breed,Color,Species,Birthdate,Sex,MicrochipId,Description")] Pet pet)
+        {
+            if (ModelState.IsValid)
+            {
+                pet.Id = Guid.NewGuid();
+                _context.Add(pet);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(pet);
+        }
+
+        // GET: Pets/Edit/5
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var pet = await _context.Pets.FindAsync(id);
+            if (pet == null)
+            {
+                return NotFound();
+            }
+            return View(pet);
+        }
+
+        // POST: Pets/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, [Bind("Id,Name,Breed,Color,Species,Birthdate,Sex,MicrochipId,Description")] Pet pet)
+        {
+            if (id != pet.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(pet);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PetExists(pet.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(pet);
+        }
+
+        // GET: Pets/Delete/5
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var pet = await _context.Pets
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (pet == null)
+            {
+                return NotFound();
+            }
+
+            return View(pet);
+        }
+
+        // POST: Pets/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            var pet = await _context.Pets.FindAsync(id);
+            if (pet != null)
+            {
+                _context.Pets.Remove(pet);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PetExists(Guid id)
+        {
+            return _context.Pets.Any(e => e.Id == id);
+        }
+    }
+}
--- Models/Pet.cs
+++ Models/Pet.cs
@@ -7,7 +7,12 @@ public class Pet
     public string Breed { get; set; } = "Mestizo";
     public string? Color { get; set; }
     public string? Species { get; set; }
-    public DateTime Birthdate { get; set; }
+    private DateTime _birthdate;
+    public DateTime Birthdate
+    {
+        get { return _birthdate; }
+        set { _birthdate = DateTime.SpecifyKind(value, DateTimeKind.Utc); }
+    }
     public bool Sex { get; set; }
     public string? MicrochipId { get; set; }
     public string? Description { get; set; }
