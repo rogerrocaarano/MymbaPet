bff031e18187b1d0d755a92cc7871514ef04c62e
Róger Roca Arano <63246718+rogerrocaarano@users.noreply.github.com>
2024-06-13 09:53:24 -0400

    ui: Actualización de layout de aplicación y formularios.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,36 +2,50 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-  content: [
-    "Views/**/*.cshtml",
-    "Areas/**/*.cshtml",
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        'sans': ['"Hind"', ...defaultTheme.fontFamily.sans],
-        'sans-header': ['"Poppins"', ...defaultTheme.fontFamily.sans],
-        'brand': ['"Boogaloo"', 'cursive'],
-        'ui': ['"Roboto"', ...defaultTheme.fontFamily.sans],
-      },
-      colors: {
-        primary: '#7C58D3',
-        secondary: '#221956',
-        terciary: '#227C9D',
-        ui: {
-          light: {
-            section: '#FFFFFF',
-            page: '#F3F3F1',
-            contrast: '#DDE1E6',
-            hover: '#F2F8F5'
-          }
-        }
-      },
+    content: [
+        "Views/**/*.cshtml",
+        "Areas/**/*.cshtml",
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                'sans': ['"Hind"', ...defaultTheme.fontFamily.sans],
+                'sans-header': ['"Poppins"', ...defaultTheme.fontFamily.sans],
+                'brand': ['"Boogaloo"', 'cursive'],
+                'ui': ['"Roboto"', ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                primary: {
+                    100: "#f4f0ff",
+                    200: "#D1D0FB",
+                    300: "#AF97EB",
+                    DEFAULT: "#986DF3",
+                    500: "#7C58D3",
+                    800: "#221956"
+                },
+                tertiary:{
+                    100: "#F2F4F8",
+                    DEFAULT: "#227C9D"
+                },
+                secondary:{
+                    100: "#FFFFFF",
+                    200: "#FEF7FF",
+                    300: "#F6F6FE",
+                    400: "#E7ECF3",
+                    500: "#C1C7CD",
+                    DEFAULT: "#E7B387",
+                    700: "#EAAC8E1A"
+                },
+                alert: {
+                    DEFAULT: "#E34D56",
+                }
+
+            },
+        },
     },
-  },
-  plugins: [
-    require('@tailwindcss/forms'),
-  ],
-  // safelist: [{ pattern: /./}]
+    plugins: [
+        require('@tailwindcss/forms'),
+    ],
+    // safelist: [{ pattern: /./}]
 }
 
